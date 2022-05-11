30877b82e996e88abbf2a6857cef4e708848a3ba
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-05-11 23:47:39 +0800

    create utils

---
+++ app/src/main/java/com/app/sehatin/utils/DateHelper.kt
@@ -0,0 +1,15 @@
+package com.app.sehatin.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+const val DATE_INPUT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+const val TIME_FORMAT = "HH:mm aaa"
+
+fun String.convertToDate(): String {
+    val inputFormat = SimpleDateFormat(DATE_INPUT_PATTERN, Locale.US)
+    val timeFormat = SimpleDateFormat(TIME_FORMAT, Locale.US)
+    val outputFormat = SimpleDateFormat.getDateInstance(SimpleDateFormat.FULL)
+    val date = inputFormat.parse(this) as Date
+    return outputFormat.format(date) + " " + timeFormat.format(date)
+}
---
+++ app/src/main/java/com/app/sehatin/utils/Validator.kt
@@ -0,0 +1,30 @@
+package com.app.sehatin.utils
+
+import android.widget.EditText
+
+object Validator {
+
+    fun isAllFormFilled(args: Array<EditText>): Boolean  {
+        for(editText in args) {
+            if(editText.text.toString().isEmpty()) {
+                return false
+            }
+        }
+        return true
+    }
+
+    fun isFormValid(args: Array<EditText>): Boolean  {
+        for(editText in args) {
+            if(editText.error != null) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private const val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+    fun isEmailValid(email: String) : Boolean {
+        return email.matches(emailPattern.toRegex())
+    }
+
+}
