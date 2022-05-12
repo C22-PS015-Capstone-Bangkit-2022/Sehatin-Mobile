b63c14258136ba22e4f3f1cb461038cfe3e32ed5
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-05-12 13:09:05 +0800

    edit layout register

--- app/src/main/java/com/app/sehatin/data/model/User.kt
+++ app/src/main/java/com/app/sehatin/data/model/User.kt
@@ -4,7 +4,9 @@ data class User(
     var id: String,
     var username: String,
     var email: String,
+    var dateOfBirth: String,
     var imageUrl: String,
+    var gender: String,
     var diseases: List<Disease>
 ) {
     companion object {
