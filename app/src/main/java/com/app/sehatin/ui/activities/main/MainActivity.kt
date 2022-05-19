f807be342179bd37b0d61717627cd2000f915bd8
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-05-19 18:55:47 +0800

    add listener to user data update in MainActivity

--- app/src/main/java/com/app/sehatin/ui/activities/main/MainActivity.kt
+++ app/src/main/java/com/app/sehatin/ui/activities/main/MainActivity.kt
@@ -2,7 +2,12 @@ package com.app.sehatin.ui.activities.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.app.sehatin.data.model.User
 import com.app.sehatin.databinding.ActivityMainBinding
+import com.app.sehatin.utils.USER_COLLECTION
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -11,5 +16,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        userId?.let {
+            FirebaseFirestore.getInstance()
+                .collection(USER_COLLECTION)
+                .document(it)
+                .addSnapshotListener { value, error ->
+                    Log.d("MainActivity", "User: $value")
+                    if(error != null) {
+                        return@addSnapshotListener
+                    }
+                    if(value != null && value.exists()) {
+                        val user = value.toObject(User::class.java)
+                        User.currentUser = user
+                    }
+                }
+        }
     }
 }
