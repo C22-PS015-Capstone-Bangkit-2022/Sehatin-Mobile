95a6ba570556d5baca2157c5fbc7c483c5fc23e9
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-05-24 14:21:52 +0800

    create ArticleAdapter

--- app/src/main/java/com/app/sehatin/ui/activities/main/fragments/content/home/viewHolder/HomeArticleHolder.kt
+++ app/src/main/java/com/app/sehatin/ui/activities/main/fragments/content/home/viewHolder/HomeArticleHolder.kt
@@ -2,7 +2,6 @@ package com.app.sehatin.ui.activities.main.fragments.content.home.viewHolder
 
 import android.content.Context
 import androidx.lifecycle.LifecycleOwner
-import com.app.sehatin.databinding.ItemArticleBinding
 import com.app.sehatin.databinding.ItemHomeArticleBinding
 import com.app.sehatin.ui.activities.main.fragments.content.home.HomeViewModel
 import com.app.sehatin.ui.sharedAdapter.ViewHolder
@@ -13,6 +12,7 @@ class HomeArticleHolder(
     private val lifecycleOwner: LifecycleOwner
 ) : ViewHolder(binding.root) {
     private lateinit var context: Context
+
     override fun bind(context: Context) {
         this.context = context
         initVariable()
