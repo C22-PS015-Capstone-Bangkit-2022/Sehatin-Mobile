1d3e4bca77d138a2e556e790ba3f00fa6d88ce8c
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-05-29 14:42:22 +0800

    create UserPageViewModel

---
+++ app/src/main/java/com/app/sehatin/ui/activities/main/fragments/userPage/UserPageViewModel.kt
@@ -0,0 +1,14 @@
+package com.app.sehatin.ui.activities.main.fragments.userPage
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.app.sehatin.data.Result
+import com.app.sehatin.data.model.Posting
+import com.app.sehatin.data.repository.PostingRepository
+
+class UserPageViewModel(private val postingRepository: PostingRepository): ViewModel() {
+
+    val userPostState = MutableLiveData<Result<List<Posting>>>()
+    fun getUserPost(userId: String) = postingRepository.getUserPost(userPostState, userId)
+
+}
