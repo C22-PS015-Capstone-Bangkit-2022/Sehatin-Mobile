1b4b99b783eb84652726fb6e00968d1ad87ad1b1
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-05-30 16:54:14 +0800

    create saved post entity

---
+++ app/src/main/java/com/app/sehatin/data/local/entity/SavedPostEntity.kt
@@ -0,0 +1,16 @@
+package com.app.sehatin.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = SavedPostEntity.TABLE_NAME)
+data class SavedPostEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+
+    val postId: String
+) {
+    companion object {
+        const val TABLE_NAME = "saved_post"
+    }
+}
