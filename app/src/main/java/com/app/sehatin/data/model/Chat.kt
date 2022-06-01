766b5b6089fbe25af7d1a6d3d9464c9ebf0f409e
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-06-02 07:57:55 +0800

    create method for chat in repostory

--- app/src/main/java/com/app/sehatin/data/model/Chat.kt
+++ app/src/main/java/com/app/sehatin/data/model/Chat.kt
@@ -1,15 +1,10 @@
 package com.app.sehatin.data.model
 
-data class HistoryChat(
+data class Chat(
     val id: String,
     val server_timestamp: String,
     val createdAt: String,
     val sender: String,
     val receiver: String,
     val message: String,
-    val withUser: String,
-) {
-    companion object {
-        const val CREATED_AT = "createdAt"
-    }
-}
+)
--- app/src/main/java/com/app/sehatin/data/repository/ChatRepository.kt
+++ app/src/main/java/com/app/sehatin/data/repository/ChatRepository.kt
@@ -2,6 +2,7 @@ package com.app.sehatin.data.repository
 
 import androidx.lifecycle.MutableLiveData
 import com.app.sehatin.data.Result
+import com.app.sehatin.data.model.Chat
 import com.app.sehatin.data.model.HistoryChat
 import com.app.sehatin.utils.CHAT_REFERENCE
 import com.app.sehatin.utils.HISTORY_CHAT_REFERENCE
@@ -14,6 +15,41 @@ import com.google.firebase.ktx.Firebase
 class ChatRepository {
     private val chatRef = Firebase.database.reference.child(CHAT_REFERENCE)
     private val historyChatRef = Firebase.database.reference.child(HISTORY_CHAT_REFERENCE)
+
+    //SEND CHAT
+
+    fun sendChat(userId: String, chat: Chat) {
+        chatRef.child(userId)
+            .setValue(chat)
+    }
+
+    fun getChat(getChatState: MutableLiveData<Result<List<Chat>>>, userId: String) {
+        getChatState.value = Result.Loading
+        chatRef.child(userId)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val chats = mutableListOf<Chat>()
+                    for(snap in snapshot.children) {
+                        val chat = snap.getValue(Chat::class.java)
+                        if(chat != null) {
+                            chats.add(chat)
+                        }
+                    }
+                    getChatState.value = Result.Success(chats)
+                }
+                override fun onCancelled(error: DatabaseError) {
+                    getChatState.value = Result.Error(error.message)
+                }
+            })
+    }
+
+    // CHAT HISTORY
+
+    fun addChatHistory(userId: String, historyChat: HistoryChat) {
+        historyChatRef
+            .child(userId)
+            .setValue(historyChat)
+    }
 
     fun getUserChatHistory(historyChatState: MutableLiveData<Result<List<HistoryChat>>>, userId: String) {
         historyChatState.value = Result.Loading
