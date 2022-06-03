1fb0ada6a182df2bcd1554322964814cc4861273
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-06-03 21:09:11 +0800

    show list doctor

--- app/src/main/java/com/app/sehatin/ui/activities/main/fragments/content/fragments/consultation/ConsultationFragment.kt
+++ app/src/main/java/com/app/sehatin/ui/activities/main/fragments/content/fragments/consultation/ConsultationFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.sehatin.data.Result
 import com.app.sehatin.databinding.FragmentConsultationBinding
 import com.app.sehatin.ui.viewmodel.ViewModelFactory
 
 class ConsultationFragment : Fragment() {
     private lateinit var binding: FragmentConsultationBinding
     private lateinit var viewModel: ConsultationViewModel
+    private val doctorAdapter = DoctorAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentConsultationBinding.inflate(inflater, container, false)
@@ -25,10 +28,35 @@ class ConsultationFragment : Fragment() {
         viewModel = ViewModelProvider(this@ConsultationFragment, ViewModelFactory.getInstance())[ConsultationViewModel::class.java]
         rvListDoctor.setHasFixedSize(true)
         rvListDoctor.layoutManager = LinearLayoutManager(requireContext())
+        rvListDoctor.adapter = doctorAdapter
     }
 
     private fun initListener() {
+        viewModel.getDoctor().observe(viewLifecycleOwner) {
+            when(it) {
+                is Result.Loading -> {
+                    showLoading(true)
+                }
+                is Result.Error -> {
+                    showLoading(false)
+                    Toast.makeText(requireContext(), it.error, Toast.LENGTH_SHORT).show()
+                }
+                is Result.Success -> {
+                    showLoading(false)
+                    doctorAdapter.submitList(it.data)
+                }
+            }
+        }
+    }
 
+    private fun showLoading(isLoading: Boolean) = with(binding) {
+        if(isLoading) {
+            progressBar.visibility = View.VISIBLE
+            rvListDoctor.visibility = View.GONE
+        } else {
+            progressBar.visibility = View.GONE
+            rvListDoctor.visibility = View.VISIBLE
+        }
     }
 
 }
