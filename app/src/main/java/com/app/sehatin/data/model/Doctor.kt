5dd6c66ab29221e4ab68bca6a179c1e7de4f0fcf
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-06-03 20:40:46 +0800

    create doctor model

---
+++ app/src/main/java/com/app/sehatin/data/model/Doctor.kt
@@ -0,0 +1,15 @@
+package com.app.sehatin.data.model
+
+data class Doctor(
+    val id: String? = null,
+    val name: String? = null,
+    val strNumber: Int? = null,
+    val specialist: String? = null,
+    val experience_year: Int? = null,
+    val alumnus: String? = null,
+    val practice_at: String? = null,
+    val price: String? = null,
+    val ratings: Float? = null,
+    val review: Int? = null,
+    val imageUrl: String? = null,
+)
