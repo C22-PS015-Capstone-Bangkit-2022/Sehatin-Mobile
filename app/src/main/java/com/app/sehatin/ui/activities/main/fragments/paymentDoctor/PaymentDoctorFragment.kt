0b5ae23360512ed046e8f33f8f43f388fb006119
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-06-04 13:10:50 +0800

    create PaymentMethodAdapter

--- app/src/main/java/com/app/sehatin/ui/activities/main/fragments/paymentDoctor/PaymentDoctorFragment.kt
+++ app/src/main/java/com/app/sehatin/ui/activities/main/fragments/paymentDoctor/PaymentDoctorFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.sehatin.R
 import com.app.sehatin.data.model.Doctor
+import com.app.sehatin.data.model.PaymentMethod
 import com.app.sehatin.databinding.FragmentPaymentDoctorBinding
 import com.app.sehatin.utils.toCurrencyFormat
 import com.bumptech.glide.Glide
@@ -16,6 +18,8 @@ class PaymentDoctorFragment : Fragment() {
     private lateinit var binding: FragmentPaymentDoctorBinding
     private lateinit var doctor: Doctor
     private lateinit var viewModel: PaymentDoctorViewModel
+    private lateinit var paymentMethodAdapter: PaymentMethodAdapter
+    private var selectedPaymentMethod: PaymentMethod? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentPaymentDoctorBinding.inflate(inflater, container, false)
@@ -28,12 +32,19 @@ class PaymentDoctorFragment : Fragment() {
         viewModel = ViewModelProvider(this@PaymentDoctorFragment)[PaymentDoctorViewModel::class.java]
         doctor = PaymentDoctorFragmentArgs.fromBundle(arguments as Bundle).doctor
         totalPayment.text = doctor.price?.toCurrencyFormat()
+        paymentMethodAdapter = PaymentMethodAdapter {
+            selectedPaymentMethod = it
+            payButton.isEnabled = true
+        }
         initDoctorInfo()
         initPaymentInfo()
+        initPaymentMethod()
     }
 
-    private fun initListener() {
-
+    private fun initListener() = with(binding) {
+        toolbar.setNavigationOnClickListener {
+            requireActivity().onBackPressed()
+        }
     }
 
     //INIT VIEW
@@ -51,6 +62,13 @@ class PaymentDoctorFragment : Fragment() {
         doctorPrice.text = doctor.price?.toCurrencyFormat()
         discountAmount.text = "-"
         totalPrice.text = doctor.price?.toCurrencyFormat()
+    }
+
+    private fun initPaymentMethod() = with(binding.paymentMethod) {
+        rvPaymentMethod.setHasFixedSize(true)
+        rvPaymentMethod.layoutManager = LinearLayoutManager(requireContext())
+        rvPaymentMethod.adapter = paymentMethodAdapter
+        paymentMethodAdapter.submitList(viewModel.getPaymentMethod())
     }
 
 }
---
+++ app/src/main/java/com/app/sehatin/ui/activities/main/fragments/paymentDoctor/PaymentMethodAdapter.kt
@@ -0,0 +1,58 @@
+package com.app.sehatin.ui.activities.main.fragments.paymentDoctor
+
+import android.annotation.SuppressLint
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.app.sehatin.data.model.PaymentMethod
+import com.app.sehatin.databinding.ItemPaymentMethodBinding
+import com.bumptech.glide.Glide
+
+class PaymentMethodAdapter(private val onMethodSelected: (PaymentMethod) -> Unit): ListAdapter<PaymentMethod, PaymentMethodAdapter.Holder>(DIFF_CALLBACK) {
+    private var selectedPosition = -1
+
+
+    inner class Holder(private val binding : ItemPaymentMethodBinding): RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("NotifyDataSetChanged")
+        fun bind(paymentMethod: PaymentMethod) = with(binding) {
+            radioBtn.isChecked = bindingAdapterPosition == selectedPosition
+            Glide.with(this.root)
+                .load(paymentMethod.imageUrl)
+                .into(paymentImage)
+            paymentName.text = paymentMethod.name
+            this.root.setOnClickListener {
+                selectedPosition = bindingAdapterPosition
+                notifyDataSetChanged()
+                onMethodSelected(paymentMethod)
+            }
+            radioBtn.isClickable = false
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = ItemPaymentMethodBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return Holder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        val DIFF_CALLBACK: DiffUtil.ItemCallback<PaymentMethod> =
+            object : DiffUtil.ItemCallback<PaymentMethod>() {
+                override fun areItemsTheSame(oldUser: PaymentMethod, newUser: PaymentMethod): Boolean {
+                    return oldUser.name == newUser.name
+                }
+
+                @SuppressLint("DiffUtilEquals")
+                override fun areContentsTheSame(oldUser: PaymentMethod, newUser: PaymentMethod): Boolean {
+                    return oldUser == newUser
+                }
+            }
+    }
+
+}
