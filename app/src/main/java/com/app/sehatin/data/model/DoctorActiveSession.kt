e95fc569d2350c61de3aa78c685019c0297944e0
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-06-04 18:04:53 +0800

    create DoctorActiveSession

---
+++ app/src/main/java/com/app/sehatin/data/model/DoctorActiveSession.kt
@@ -0,0 +1,8 @@
+package com.app.sehatin.data.model
+
+data class DoctorActiveSession(
+    val id: String? = null,
+    val doctorId: String? = null,
+    val validUntil: String? = null,
+    val createdAt: String? = null,
+)
