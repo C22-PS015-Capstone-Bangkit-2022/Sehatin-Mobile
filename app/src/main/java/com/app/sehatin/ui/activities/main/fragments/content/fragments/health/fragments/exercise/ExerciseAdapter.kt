a303b99c99e1648e876c9986dda71a28a2d0f60e
user <ctanbunan@gmail.com>
2022-06-09 23:08:35 +0700

    ExerciseAdapter.kt item_exercise.xml

--- app/src/main/java/com/app/sehatin/ui/activities/main/fragments/content/fragments/health/fragments/exercise/ExerciseAdapter.kt
+++ app/src/main/java/com/app/sehatin/ui/activities/main/fragments/content/fragments/health/fragments/exercise/ExerciseAdapter.kt
@@ -4,22 +4,22 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.app.sehatin.data.model.Exercise
-import com.app.sehatin.databinding.ItemFoodBinding
+import com.app.sehatin.databinding.ItemExerciseBinding
 import com.bumptech.glide.Glide
 
 class ExerciseAdapter(private val exercises: List<Exercise>): RecyclerView.Adapter<ExerciseAdapter.Holder>() {
-    private lateinit var binding: ItemFoodBinding
-    inner class Holder(binding: ItemFoodBinding): RecyclerView.ViewHolder(binding.root) {
+    private lateinit var binding: ItemExerciseBinding
+    inner class Holder(binding: ItemExerciseBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(exercise: Exercise) = with(binding) {
             Glide.with(this.root)
                 .load(exercise.thumbnail)
-                .into(foodImage)
-            foodName.text = exercise.name
+                .into(exerciseImage)
+            exerciseName.text = exercise.name
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        binding = ItemFoodBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        binding = ItemExerciseBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return Holder(binding)
     }
 
